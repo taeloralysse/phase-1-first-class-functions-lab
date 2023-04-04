1bfbd8d32d3e7b3ce3885a85d772603e0591518b
Taelor Lambert <taeloralysse@gmail.com>
2023-04-04 11:33:48 -0400

    fares

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(2)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(){
+    let fareMultiplier = function(){return 25}
+    return fareMultiplier
+}
+function fareDoubler(createFareMultiplier){
+    return createFareMultiplier * 2
+}
+function fareTripler(createFareMultiplier){
+    return createFareMultiplier * 3
+}
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers)
+}
+function selectDifferentDrivers(drivers, returnLastTwoDrivers){
+    return returnLastTwoDrivers(drivers)
+}
